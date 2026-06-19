d1706b7fb789ed1e563f959d2acd9c954501cf32
alimomennasab <alimomennasab3@gmail.com>
2026-06-19 12:56:42 -0700

    wip training loop and reorganize helpers to util/model files

--- train.py
+++ train.py
@@ -1,22 +1,10 @@
 import os
+import time
 import numpy as np
 import torch
 from torch.utils.data import DataLoader, Dataset
-
-def sample_t_r(batch_size, percent_unequal=0.25):
-    # percent_unequal: fraction of batch where r != t
-    t = torch.rand(batch_size)
-    r = torch.rand(batch_size) * t  # r in [0, t], so r <= t
-
-    num_unequal = int(percent_unequal * batch_size)
-
-    unequal_mask = torch.zeros(batch_size, dtype=torch.bool)
-    unequal_indices = torch.randperm(batch_size)[:num_unequal] # randomly choose percent_unequal% of (r, t) to be unequal
-    unequal_mask[unequal_indices] = True
-
-    r = torch.where(unequal_mask, r, t)
-
-    return t, r
+from unet import UNet, sample_t_r
+from utils import plot_loss, create_experiment_dir
 
 class MeanFlowDataset(Dataset):
     """Load preprocessed 64x64x3 images from ``train_npy/{class}_npy/*.npy``."""
@@ -44,19 +32,73 @@ class MeanFlowDataset(Dataset):
 
 
 if __name__ == "__main__":
-    dataset_path = "data/imagenette2/train_npy/"
-    train_dataset = MeanFlowDataset(dataset_path)
-    print(f"num samples: {len(train_dataset)}")
+    # hyperparams & config
+    run = 0
+    epochs = 20
+    loss_fn = nn.MSELoss()
+
+    # dataset, dataloader, save path
+    save_path = create_experiment_dir(run)
+    train_dataset_path = "data/imagenette2/train_npy/"
+    val_dataset_path = "data/imagenette2/val_npy/"
+    train_dataset = MeanFlowDataset(train_dataset_path)
+    val_data = MeanFlowDataset(val_dataset_path)
+    print(f"num train samples: {len(train_dataset)}")
+    print(f"num val samples: " {len(val_dataset)})
 
     image, label = train_dataset[0]
     print(f"sample shape: {image.shape}, label: {label}")
 
     # subset of 3 samples
     #train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
-    subset = torch.utils.data.Subset(train_dataset, [0, 1, 2])
-    small_train_loader = DataLoader(subset, batch_size=3, shuffle=False)
+    #val_loader = DataLoader(val_dataset, batch_size=8, shuffle=True)
+    train_subset = torch.utils.data.Subset(train_dataset, [0, 1, 2])
+    val_subset = torch.utils.data.Subset(val_dataset, [0, 1, 2])
+    small_train_loader = DataLoader(train_subset, batch_size=3, shuffle=False)
+    small_val_loader = DataLoader(val_subset, batch_size=3, shuffle=False)
+
+    # initialize model
+    model = UNet(in_ch=3, ch=(64, 128, 256, 512), d_emb=256)
 
     # training loop
-    t, r = sample_t_r(batch_size=8)
-    print(t)
-    print(r)
+    train_start_time = time.Time()
+    train_losses, val_losses = [], []
+
+    for i in range(epochs):
+        model.train()
+        epoch_start_time = time.Time()
+        epoch_train_loss = 0
+
+        for images, labels in small_train_loader: 
+            t, r = sample_t_r(batch_size=8)
+            print("t: ", t)
+            print("r: ", r)
+
+            """
+            from paper:
+                t, r = sample_t_r()
+                e = randn_like(x)
+                z = (1 - t) * x + t * e
+                v = e - x
+                u, dudt = jvp(fn, (z, r, t), (v, 0, 1))
+                u_tgt = v - (t - r) * dudt
+                error = u - stopgrad(u_tgt)
+                loss = metric(error)
+            """
+
+            # todo get loss
+
+        model.eval()
+        epoch_val_loss = 0
+        for images, labels in small_val_loader:
+            t, r = sample_t_r(batch_size=8)
+            print("t: ", t)
+            print("r: ", r)
+
+            # todo get loss
+
+        plot_loss(train_loss, val_loss, save_path)
+        print(f"Epoch {i+1}: {time.Time() - epoch_start_time}") # todo log losses
+
+    torch.save(model.state_dict(), os.path.join(save_path, "meanflow.pt"))
+    print("Total training time: ", time.Time() - train_start_time)
